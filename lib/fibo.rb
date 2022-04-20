fcb75f082a8f25594dc29e35ecca326638c54cb5
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-20 14:35:17 -0500

    Add fib_rec to resolve fibonachi serie with recursively

--- lib/fibo.rb
+++ lib/fibo.rb
@@ -10,5 +10,15 @@ def fibs(n)
     array
 end
 
+def fibs_rec(n)
+    array = if n > 2 then fibs_rec(n-1) else return (0...n).to_a end 
+    array << array[-2] + array[-1]
+end
+
+
+
+
 puts "Use of iteration"
 puts fibs(8).inspect
+puts "Use recursively"
+p fibs_rec(8)
